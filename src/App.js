725ca1712b3f5c43d412f9dc84c6718c66a59ec5
sagegarrett14 <sage.garrett14@gmail.com>
2024-05-30 16:57:25 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<header className="App-header">
+			Sage Garrett
+		</header>
+		<body>
+			See below for details
+		</body>
+		<footer>
+			Created using React.js
+		</footer>
     </div>
   );
 }
